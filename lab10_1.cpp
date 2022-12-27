9a50d97395555c229ae0072c286d7c012a951196
aonnapaht <napaht_d@cmu.ac.t>
2022-12-27 17:28:10 +0700

    lab10_1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,10 +2,16 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double loan;
+    double rate;
+    double amount;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> amount;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -18,16 +24,46 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
+	int i = 1;
+	double prevb;
+	double intr;
+	double total;
+	double newb;
+
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	prevb = loan;
+	while(newb != 0){
+	     intr = (rate/100)*prevb;
+    	 total = prevb + intr;
+	     newb = total - amount;
+	     if(total < amount ){
+	         amount = total;
+	         newb = 0;
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << prevb;
+	cout << setw(13) << left << intr;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << amount;
+	cout << setw(13) << left << newb;
+	cout << "\n";
+	    break;
+	         
+	     }
+	cout << fixed << setprecision(2); 
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << prevb;
+	cout << setw(13) << left << intr;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << amount;
+	cout << setw(13) << left << newb;
+	cout << "\n";
+	    prevb = newb; 
+	    i++;
+	    
+	   }
 	
 	return 0;
 }
+
